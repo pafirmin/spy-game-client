367802ad228036c5120daa2eb9dab34b99f1308d
Paul <paulfirmin1@gmail.com>
2022-09-25 21:44:36 +0100

    set socketio path

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-const socket = io("/api");
+const socket = io(`${process.env.REACT_APP_API_BASE}`, { path: `${process.env.REACT_APP_SOCKET_PATH}`} );
 
 export default socket;
